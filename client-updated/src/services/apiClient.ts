107337129cf900062b1baade2f1a9a35a5b0df19
Sakhi Jindal <sakhi.jindal@gmail.com>
2025-08-06 10:28:55 +0500

    Update apiClient.ts

--- client-updated/src/services/apiClient.ts
+++ client-updated/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api/test/',
+  baseURL: process.env.REACT_APP_API_URL || 'https://health-diary-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
