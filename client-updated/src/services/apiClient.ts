f883aa9803ff9adfad08a9d484af69bb1cd73234
Sakhi Jindal <sakhi.jindal@gmail.com>
2025-08-06 09:59:44 +0500

    Update apiClient.ts

--- client-updated/src/services/apiClient.ts
+++ client-updated/src/services/apiClient.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_API_URL;
-console.log("REACT_APP_API_URL", REACT_APP_API_URL);
 
 const api = axios.create({
   baseURL: BASE_URL || 'http://localhost:5000/api/test/',
